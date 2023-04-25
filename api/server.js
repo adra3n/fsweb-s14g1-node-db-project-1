1389b888580c9d1f180d5a13064ee2b8a5643cca
Sertaç Kocagil <srtc.kcgl@gmail.com>
2023-04-25 21:20:58 +0300

    wip

--- api/server.js
+++ api/server.js
@@ -1,7 +1,8 @@
-const express = require("express");
+const express = require('express')
+const router = require('./accounts/accounts-router')
+const server = express()
 
-const server = express();
+server.use(express.json())
+server.use('/api/accounts', router)
 
-server.use(express.json());
-
-module.exports = server;
+module.exports = server
--- queries.sql
+++ queries.sql
@@ -2,11 +2,11 @@
 
 -- Posta kodu 1010 olan tüm müşterileri bulun
 
-SELECT * FROM [Customers] WHERE PostalCode=1010
+SELECT * FROM [Customers] WHERE PostalCode=1010;
 
 -- id'si 11 olan tedarikçinin telefon numarasını bulun
 
-SELECT Phone FROM [Suppliers] WHERE SupplierID=11 
+SELECT Phone FROM [Suppliers] WHERE SupplierID=11;
 
 -- Verilen ilk 10 siparişi, sipariş tarihine göre azalan şekilde listeleyin
 
@@ -14,7 +14,10 @@ SELECT * FROM [Orders] ORDER BY OrderDate DESC LIMIT 10;
 
 -- Londra, Madrid veya Brezilya'da yaşayan tüm müşterileri bulun
 
-SELECT * FROM [Customers] WHERE Country like "Brazil" or City like "London" or City like "Madrid"
+SELECT * FROM [Customers] WHERE Country like "Brazil" or City like "London" or City like "Madrid";
+
+SELECT * FROM [Customers] WHERE Country like "Brazil" or City IN ("London", "Madrid")
+
 
 -- "The Shire" için bir müşteri kaydı ekleyin, ilgili kişi adı "Bilbo Baggins", adres - "Bag End" içinde "1 Hobbit-Hole", posta kodu "111" ve ülke "Middle Earth"
 
@@ -23,11 +26,11 @@ VALUES  ('The Shire','Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle
 
 -- Posta kodu "11122" olarak değişecek şekilde Bilbo Baggins kaydını güncelleyin
 
-UPDATE [Customers] SET PostalCode="11122" WHERE ContactName like "Bilbo Baggins" 
+UPDATE [Customers] SET PostalCode="11122" WHERE ContactName like "Bilbo Baggins";
 
 -- (Zorlayıcı Görev) Müşteriler tablosunda kaç farklı şehrin saklandığını keşfetmek için bir sorgu bulun. Tekrarlar çift sayılmamalıdır
 
-SELECT COUNT(DISTINCT City) AS X FROM Customers;
+SELECT COUNT(DISTINCT City) AS CitiesCount FROM Customers;
 
 -- (Zorlayıcı Görev) 20 karakterden uzun adları olan tüm tedarikçileri bulun. Adın uzunluğunu almak için "length(SupplierName)" kullanabilirsiniz.
 
