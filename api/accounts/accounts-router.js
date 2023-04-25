182dd6c0f51f1084aab0243a6b882bba2d0524ec
Sertaç Kocagil <srtc.kcgl@gmail.com>
2023-04-25 21:37:02 +0300

    Mvp done. Tests passed.

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,79 @@
 const router = require('express').Router()
+const accountsModel = require('./accounts-model')
+const middleware = require('./accounts-middleware')
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
   // KODLAR BURAYA
+  try {
+    const allAccounts = await accountsModel.getAll()
+    res.json(allAccounts)
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', middleware.checkAccountId, (req, res, next) => {
   // KODLAR BURAYA
+  try {
+    res.json(req.Account)
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.post('/', (req, res, next) => {
+router.post(
+  '/',
+  middleware.checkAccountPayload,
+  middleware.checkAccountNameUnique,
+  async (req, res, next) => {
+    // KODLAR BURAYA
+    try {
+      let model = { name: req.body.name, budget: req.body.budget }
+      const insertedAccount = await accountsModel.create(model)
+
+      res.status(201).json(insertedAccount)
+    } catch (error) {
+      next(error)
+    }
+  }
+)
+
+router.put(
+  '/:id',
+  middleware.checkAccountId,
+  middleware.checkAccountPayload,
+  middleware.checkAccountNameUnique,
+  async (req, res, next) => {
+    // KODLAR BURAYA
+    try {
+      let model = { name: req.body.name, budget: req.body.budget }
+      const updatedAccount = await accountsModel.updateById(
+        req.params.id,
+        model
+      )
+      res.json(updatedAccount)
+    } catch (error) {
+      next(error)
+    }
+  }
+)
+
+router.delete('/:id', middleware.checkAccountId, async (req, res, next) => {
   // KODLAR BURAYA
+  try {
+    await accountsModel.deleteById(req.params.id)
+    res.json(`${req.Account.name} isimli kayıt silindi`)
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.put('/:id', (req, res, next) => {
+router.use((err, req, res, next) => {
   // KODLAR BURAYA
-});
-
-router.delete('/:id', (req, res, next) => {
-  // KODLAR BURAYA
+  res.status(err.status || 400).json({
+    customMessage: 'Custom error handler üzerinden hata alındı',
+    message: err.message,
+  })
 })
 
-router.use((err, req, res, next) => { // eslint-disable-line
-  // KODLAR BURAYA
-})
-
-module.exports = router;
+module.exports = router
